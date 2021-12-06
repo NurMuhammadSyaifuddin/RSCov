a04206aa3fd0be381945df50a316e5fae420326c
NurMuhammadSyaifuddin <nurm9301@gmail.com>
2021-12-06 09:15:00 +0700

    Added Filter function on MainAdapter.kt

--- app/src/main/java/com/project/rscov/adapter/MainAdapter.kt
+++ app/src/main/java/com/project/rscov/adapter/MainAdapter.kt
@@ -2,6 +2,8 @@ package com.project.rscov.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.project.rscov.R
@@ -10,9 +12,10 @@ import com.project.rscov.model.Hospital
 import com.project.rscov.utils.DivHospitalsCallback
 import com.project.rscov.utils.loadImage
 
-class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>(), Filterable {
 
     private var listener: ((Hospital) -> Unit)? = null
+    private var hospitalsFilter = mutableListOf<Hospital>()
 
     var hospitals = mutableListOf<Hospital>()
     set(value) {
@@ -20,7 +23,40 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         val result = DiffUtil.calculateDiff(callback)
         field.clear()
         field.addAll(value)
+        hospitalsFilter.addAll(value)
         result.dispatchUpdatesTo(this)
+    }
+
+    private val filters = object : Filter(){
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            var filteredList = mutableListOf<Hospital>()
+            val filterPattern = constraint.toString().trim().lowercase()
+
+            if (filterPattern.isEmpty()){
+                filteredList = hospitals
+            }else{
+                for (hospital in hospitals){
+                    val title = hospital.name.trim().lowercase()
+                    val region = hospital.region.trim().lowercase()
+                    val province = hospital.province.trim().lowercase()
+
+                    if (title.contains(filterPattern) || region.contains(filterPattern) || province.contains(filterPattern)){
+                        filteredList.add(hospital)
+                    }
+                }
+            }
+            val results = FilterResults()
+            results.values = filteredList
+            return results
+        }
+
+        override fun publishResults(p0: CharSequence?, results: FilterResults?) {
+            val callback = DivHospitalsCallback(hospitalsFilter, results?.values as List<Hospital>)
+            val result = DiffUtil.calculateDiff(callback)
+            hospitalsFilter = results.values as MutableList<Hospital>
+            result.dispatchUpdatesTo(this@MainAdapter)
+        }
+
     }
 
     inner class ViewHolder(private val binding: ItemHospitalBinding): RecyclerView.ViewHolder(binding.root) {
@@ -55,5 +91,7 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     fun onClick(listener: ((Hospital) -> Unit)?){
         this.listener = listener
     }
+
+    override fun getFilter(): Filter = filters
 
 }
